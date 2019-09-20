b58c8c11d71a255cfcbb1db75fba88c6b6e75d30
Matt <mame6897@colorado.edu>
2019-09-19 21:55:45 -0600

    Cleaned up list appends

--- Zoo.java
+++ Zoo.java
@@ -7,36 +7,21 @@ public class Zoo {
 	private Zookeeper keeper;
 
 	public Zoo() {
-		Dog dog1 = new Dog("Doug");
-		this.animals.add(dog1);
-		Dog dog2 = new Dog("Dimmadome");
-		this.animals.add(dog2);
-		Wolf wolf1 = new Wolf("Warren");
-		this.animals.add(wolf1);
-		Wolf wolf2 = new Wolf("Wally");
-		this.animals.add(wolf2);
-		Cat cat1 = new Cat("Carly");
-		this.animals.add(cat1);
-		Cat cat2 = new Cat("Courtney");
-		this.animals.add(cat2);
-		Lion lion1 = new Lion("Leo");
-		this.animals.add(lion1);
-		Lion lion2 = new Lion("Leopold");
-		this.animals.add(lion2);
-		Rhino rhino1 = new Rhino("Raleigh");
-		this.animals.add(rhino1);
-		Rhino rhino2 = new Rhino("Rory");
-		this.animals.add(rhino2);
-		Rhino rhino3 = new Rhino("Reginald");
-		this.animals.add(rhino3);
-		Elephant elephant1 = new Elephant("Ellie");
-		this.animals.add(elephant1);
-		Elephant elephant2 = new Elephant("Earl");
-		this.animals.add(elephant2);
-		Hippo hippo1 = new Hippo("Holly");
-		this.animals.add(hippo1);
-		Hippo hippo2 = new Hippo("Harry");
-		this.animals.add(hippo2);
+		this.animals.add(new Dog("Doug"));
+		this.animals.add(new Dog("Dimmadome"));
+		this.animals.add(new Wolf("Warren"));
+		this.animals.add(new Wolf("Wally"));
+		this.animals.add(new Cat("Carly"));
+		this.animals.add(new Cat("Courtney"));
+		this.animals.add(new Lion("Leo"));
+		this.animals.add(new Lion("Leopold"));
+		this.animals.add(new Rhino("Raleigh"));
+		this.animals.add(new Rhino("Rory"));
+		this.animals.add(new Rhino("Reginald"));
+		this.animals.add(new Elephant("Ellie"));
+		this.animals.add(new Elephant("Earl"));
+		this.animals.add(new Hippo("Holly"));
+		this.animals.add(new Hippo("Harry"));
 
 		this.keeper = new Zookeeper(this.animals);
 	}
