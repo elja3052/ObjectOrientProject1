6db998462b59567284d6ffad8205dd6a6607ac57
Matt <mame6897@colorado.edu>
2019-09-19 19:30:06 -0600

    Added references to Feline

--- Feline.java
+++ Feline.java
@@ -5,14 +5,6 @@ import java.uitl.*;
 
 public abstract class Feline extends Animal {
 
-    ArrayList<String> behaviors = new ArrayList<String>(
-            Arrays.asList(
-                    this.name + "," + " the feline paces around...",
-                    this.name + "," + " the feline stretches.",
-                    this.name + "," + " the feline chases its tail.",
-                    this.name + "," + " the feline plays with a toy."
-            ));
-
     public Feline(String name) {
         super(name);
     }
@@ -21,6 +13,17 @@ public abstract class Feline extends Animal {
     public void roam() {
 
         if (!this.is_sleep) {
+
+            ArrayList<String> behaviors = new ArrayList<String>(
+                    Arrays.asList(
+                            this.name + "," + " the feline paces around...",
+                            this.name + "," + " the feline stretches.",
+                            this.name + "," + " the feline chases its tail.",
+                            this.name + "," + " the feline plays with a toy."
+                    ));
+
+            // Reference used: Stephen Ostermiller on Stack Overflow
+            // https://stackoverflow.com/questions/8065532/how-to-randomly-pick-an-element-from-an-array
             Random rand = new Random();
 
             String rand_behavior = this.behaviors[rand.nextInt(behaviors.length)];
