8124701af7a214fc5f4a2fc6988ea7a1ef37ef31
Eli <elja3052@colorado.edu>
2019-09-19 18:25:26 -0600

    Added animal class structure

--- Animal.java
+++ Animal.java
@@ -1,5 +1,32 @@
-public class Animal {
+public abstract class Animal {
+
+	// MEMBERS
+	protected Boolean is_sleep;
+	public String name;
 
 
-	
+	// CONSTRUCTOR
+	public Animal(String name){
+		this.name = name
+	}
+
+
+	// METHODS
+	public void sleep() {
+		if (!this.is_sleep){
+			this.is_sleep = True;
+		}
+	}
+
+	public void wakeup() {
+		if (this.is_sleep){
+			this.is_sleep = False;
+		}
+	}
+
+	public abstract void eat(String food);
+
+	public abstract void roam();
+
+	public abstract void makeNoise();
 }
